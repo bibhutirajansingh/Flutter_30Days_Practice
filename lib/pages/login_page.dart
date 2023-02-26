5ddf44153cf7a50625e503a716f2860c5fa4fc2e
bibhutirajan.manojkumar <bibhutirajan.manojkumar@urjanet.com>
2023-02-26 15:41:42 +0530

    day4 completed

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,18 +1,72 @@
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(
-          child: Text(
-        "Login Page",
-        style: TextStyle(
-          fontSize: 20,
-          color: Colors.blue,
-          fontWeight: FontWeight.bold,
-        ),
-      )),
-    );
+        color: Colors.white,
+        child: Column(
+          children: [
+            Image.asset(
+              "assets/images/login_image.png",
+              fit: BoxFit.cover,
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Text(
+              "Welcome",
+              style: TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 32.0),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                      hintText: "Enter Username",
+                      labelText: "Username",
+                    ),
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      hintText: "Enter Password",
+                      labelText: "Password",
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                print("Hi Bibhuti");
+              },
+              child: Text("Login"),
+              style: TextButton.styleFrom(),
+            )
+          ],
+        ));
   }
 }
+
+
+      // child: Center(
+      //     child: Text(
+      //   "Login Page",
+      //   style: TextStyle(
+      //     fontSize: 20,
+      //     color: Colors.blue,
+      //     fontWeight: FontWeight.bold,
+      //   ),
+      // )),
